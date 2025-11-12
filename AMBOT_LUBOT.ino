7b5e7f002b2a1ac805dcc51554321f2164e00d1e
Extreme-Seventh <sherwin.escarlan@gmail.com>
2025-11-12 21:08:40 +0800

    Eyes Idle added and changed to SSD1306 Library

--- AMBOT_LUBOT.ino
+++ AMBOT_LUBOT.ino
@@ -1,18 +1,17 @@
 // EYES
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
+#include <Adafruit_SSD1306.h>
 
 #define i2c_Address 0x3c
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1   //   QT-PY / XIAO
-Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #include <FluxGarage_RoboEyes.h>
-RoboEyes<Adafruit_SH1106G> roboEyes(display); // create RoboEyes instance
+RoboEyes<Adafruit_SSD1306> roboEyes(display); 
 
 // END EYES 
 
@@ -39,6 +38,12 @@ bool lostFrame;
 Servo motor1;
 Servo motor2;
 
+unsigned long prevInputTime =  millis();
+
+const long idleInterval = 10000;
+bool stickInput = false;
+bool idleMode = false;
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
@@ -51,28 +56,64 @@ void setup() {
   motor2.attach(MOTOR2_PIN, 1000, 2000);
 
   // Startup robo eyes
-  display.begin(i2c_Address, true); // Address 0x3C default
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
   roboEyes.begin(SCREEN_WIDTH, SCREEN_HEIGHT, 100); // screen-width, screen-height, max framerate
-  roboEyes.setAutoblinker(ON, 3, 2); // Start auto blinker animation cycle -> bool active, int interval, int variation -> turn on/off, set interval between each blink in full seconds, set range for random interval variation in full seconds
-  roboEyes.setIdleMode(ON, 2, 2);
-
+  roboEyes.setAutoblinker(ON, 3, 2); 
+  
+  roboEyes.setCuriosity(ON);
   Serial.println("Setup complete");
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
 
   if (r88.read(&channels[0], &failSafe, &lostFrame)) {
     int ch2Raw = channels[1];  // CH2 RIGHT STICK FORWARD/BACK
     int ch4Raw = channels[3];  // CH4 LEFT STICK LEFT/RIGHT
 
-    int leftPulse = map(ch4Raw, 172, 1811, 1000, 2000);
-    int rightPulse = map(ch2Raw, 172, 1811, 1000, 2000);
+    int leftPulse = map(ch4Raw, 172, 1809, 1000, 2000);
+    int rightPulse = map(ch2Raw, 172, 1809, 1000, 2000);
 
     // Send pulses to ESCs
     motor1.writeMicroseconds(leftPulse);
     motor2.writeMicroseconds(rightPulse);
 
-    Serial.printf("CH2:%d CH4:%d Left:%d Right:%d\n", ch2Raw, ch4Raw, leftPulse, rightPulse);
+    if (ch4Raw >= 1100) {      
+      roboEyes.setPosition(W);            
+    } else if (ch4Raw <= 858) {      
+      roboEyes.setPosition(E);      
+    } else {
+      if (!idleMode) {
+        roboEyes.setPosition(DEFAULT);            
+      }
+    }
+
+    if (ch2Raw >= 1500) {      
+      roboEyes.setMood(ANGRY);      
+    } else {
+      roboEyes.setMood(DEFAULT);      
+    }
+
+    if ((ch2Raw >= 1000 || ch2Raw <= 990) || (ch4Raw >= 1000 || ch4Raw <= 975)) {
+      stickInput = true;
+      idleMode = false;      
+      prevInputTime = millis();      
+      roboEyes.setIdleMode(OFF, 2, 2);  
+    } else {            
+      stickInput = false;      
+    }
+
+    if (!stickInput && (currentMillis - prevInputTime >= idleInterval) ) {            
+      idleMode = true;         
+      roboEyes.setIdleMode(ON, 2, 2);
+    }   
+
+    Serial.printf("CH2:%d CH4:%d Left:%d Right:%d Input: %s IdleMode: %s\n  ", 
+                ch2Raw, ch4Raw, leftPulse, rightPulse, stickInput ? "True" : "False",
+                idleMode ? "True" : "False");  // Debugging Purposes
   }
   roboEyes.update();
 }
